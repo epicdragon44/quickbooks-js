b6797cfeba5b6190b8d9df08c8e28f990a490bfd
Daniel Wei <dominum15@outlook.com>
2021-04-08 15:25:36 -0500

    Fix attempt 1.5

--- bin/qbXMLHandler/index.js
+++ bin/qbXMLHandler/index.js
@@ -13,8 +13,8 @@
 'use strict';
 var data2xml = require('data2xml');
 var convert = data2xml({
-        xmlHeader: '<?xml version="1.0" encoding="utf-8"?>\n<?qbxml version="13.0"?>\n'
-    });
+    xmlHeader: '<?xml version="1.0" encoding="utf-8"?>\n<?qbxml version="13.0"?>\n'
+});
 
 var fs = require('fs'),
 path = require('path'),
